77c72e5ba519171f09c1461e2e4be0716e98aa4d
Gwynne Raskind <gwynne@darkrainfall.org>
2021-11-30 13:05:42 -0600

    Add stuff to the test workflow to test that code coverage ignores work.

--- sample-testable-package/Package.swift
+++ sample-testable-package/Package.swift
@@ -4,12 +4,14 @@ import PackageDescription
 let package = Package(
     name: "sample-testable-package",
     products: [
-        .library(name: "sample-testable-package", targets: ["SampleTestablePackage"]),
+        .library(name: "SampleTestablePackage", targets: ["SampleTestablePackage"]),
+        .library(name: "SampleTestablePackageBenchmark", targets: ["SampleTestablePackageBenchmark"]),
     ],
     dependencies: [
     ],
     targets: [
         .target(name: "SampleTestablePackage", dependencies: []),
+        .target(name: "SampleTestablePackageBenchmark", dependencies: [.target(name: "SampleTestablePackage")]),
         .testTarget(name: "SampleTestablePackageTests", dependencies: [.target(name: "SampleTestablePackage")]),
     ]
 )
---
+++ sample-testable-package/Sources/SampleTestablePackageBenchmark/Benchmarker.swift
@@ -0,0 +1,9 @@
+import SampleTestablePackage
+
+public final class Benchmarker {
+	public init() {}
+	
+	public func benchmarker() {
+		print("What a name for a shared test suite.")
+	}
+}
